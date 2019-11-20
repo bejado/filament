5ec0ced90c064d0c6920a0877b9cdfa79a500f0d
Benjamin Doherty <bendoherty@google.com>
2019-11-20 15:53:23 -0800

    Artifacts -> assets

--- build/common/upload-assets.py
+++ build/common/upload-assets.py
@@ -16,18 +16,18 @@ from github import Github
 import os, sys
 
 def print_usage():
-    print('Upload artifacts to a Filament GitHub release.')
+    print('Upload assets to a Filament GitHub release.')
     print()
     print('Usage:')
-    print('  upload-artifacts.py <tag> <artifact>...')
+    print('  upload-assets.py <tag> <asset>...')
     print()
     print('Notes:')
     print('  The GitHub release must already be created prior to running this script. This is typically done')
     print('  through the GitHub web UI.')
     print()
-    print('  <tag> is the Git tag for the desired release to attach artifacts to, for example, "v1.4.2".')
+    print('  <tag> is the Git tag for the desired release to attach assets to, for example, "v1.4.2".')
     print()
-    print('  <artifact> is a path to the asset file to upload. The file name will be used as the name of the')
+    print('  <asset> is a path to the asset file to upload. The file name will be used as the name of the')
     print('  asset.')
     print()
     print('  The GITHUB_API_KEY environment variable must be set to a valid authentication token for a')
@@ -39,7 +39,7 @@ if len(sys.argv) < 3:
     sys.exit(1)
 
 tag = sys.argv[1]
-artifacts = sys.argv[2:]
+assets = sys.argv[2:]
 
 authentication_token = os.environ.get('GITHUB_API_KEY')
 if not authentication_token:
@@ -67,9 +67,9 @@ if not release:
 
 print(f"Found release with tag '{tag}'.")
 
-for artifact_path in artifacts:
-    sys.stdout.write(f'Uploding artifact: {artifact_path}... ')
-    asset_name = os.path.basename(artifact_path)
-    asset = release.upload_asset(artifact_path, name=asset_name)
+for asset_path in assets:
+    sys.stdout.write(f'Uploding asset: {asset_path}... ')
+    asset_name = os.path.basename(asset_path)
+    asset = release.upload_asset(asset_path, name=asset_name)
     if asset:
         sys.stdout.write('Success!\n')
